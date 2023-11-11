3c5366e9ff15c914af42c5de5567e7f8d5d6c056
shushu <shushu@hanfu.la>
2023-11-11 20:33:25 +0800

    48. Rotate Image

--- src/rotate_image.rs
+++ src/rotate_image.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 /**
  * https://leetcode.com/problems/rotate-image/
  *
@@ -8,13 +10,22 @@
  */
 #[allow(unused)]
 fn rotate(matrix: &mut Vec<Vec<i32>>) {
-    let n = matrix.len();
+    let len = matrix.len();
 
-    for x in 0..(n + 1) / 2 {
-        for y in 0..(n + 1) / 2 {
-            let mut tmp = matrix[x][y];
-            // todo
+    let mut size: usize = if len & 1 == 0 { 2 } else { 3 };
+
+    while size <= len {
+        let x = (len - size) / 2;
+
+        for y in x..=(x + size - 2) {
+            let tmp = matrix[x][y];
+            matrix[x][y] = matrix[len - y - 1][x];
+            matrix[len - y - 1][x] = matrix[len - x - 1][len - y - 1];
+            matrix[len - x - 1][len - y - 1] = matrix[y][len - x - 1];
+            matrix[y][len - x - 1] = tmp;
         }
+
+        size += 2;
     }
 }
 
