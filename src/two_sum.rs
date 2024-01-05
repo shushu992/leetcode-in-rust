ed27a7a6b32ea0d7c348580cbbf8088dcbe04539
shushu <shushu@hanfu.la>
2024-01-05 13:58:51 +0800

    1. Two Sum

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -10,21 +10,19 @@ use std::ops::Rem;
  */
 #[allow(unused)]
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    assert!(2 <= nums.len() && nums.len() <= 10000);
-
-    let mut buckets: [Vec<(i32, usize)>; 4096] = std::array::from_fn(|_| vec![]);
+    let mut buckets: [Vec<(usize, i32)>; 256] = std::array::from_fn(|_| vec![]);
 
     for (pos1, &num1) in nums.iter().enumerate() {
-        let remainder = num1.rem(4096).unsigned_abs() as usize;
+        let remainder = (num1.rem(256) + 256).rem(256);
+        let complement = ((target - remainder).rem(256) + 256).rem(256);
 
-        let complement = target.abs_diff(remainder as i32).rem(4096) as usize;
-        for &(num2, pos2) in buckets[complement].iter() {
+        for &(pos2, num2) in buckets[complement as usize].iter() {
             if num1 + num2 == target {
                 return vec![pos1 as i32, pos2 as i32];
             }
         }
 
-        buckets[remainder].push((num1, pos1));
+        buckets[remainder as usize].push((pos1, num1));
     }
 
     vec![]
@@ -55,6 +53,39 @@ fn test_2() {
 #[test]
 fn test_3() {
     let mut result = two_sum([3, 3].to_vec(), 6);
+    result.sort();
+
+    let mut expect = [0, 1].to_vec();
+    expect.sort();
+
+    assert_eq!(result, expect);
+}
+
+#[test]
+fn test_4() {
+    let mut result = two_sum([-1, -2, -3, -4, -5].to_vec(), -8);
+    result.sort();
+
+    let mut expect = [2, 4].to_vec();
+    expect.sort();
+
+    assert_eq!(result, expect);
+}
+
+#[test]
+fn test_5() {
+    let mut result = two_sum([-1158379, 3950888].to_vec(), 2792509);
+    result.sort();
+
+    let mut expect = [0, 1].to_vec();
+    expect.sort();
+
+    assert_eq!(result, expect);
+}
+
+#[test]
+fn test_6() {
+    let mut result = two_sum([-2, 3].to_vec(), 1);
     result.sort();
 
     let mut expect = [0, 1].to_vec();
